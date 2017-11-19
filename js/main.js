c4c276ffc25de7023af36c6f4a88da835e0b0db5
vivien xu <lynchvivien@gmail.com>
2017-11-19 12:53:13 +1100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+var cardOne = "queen";
+console.log("User flipped " + cardOne);
+var cardTwo = "queen"
+console.log("User flipped " + cardTwo);
+var cardThree = "king";
+console.log("User flipped " + cardThree);
+var cardFour = "king";
+console.log("User flipped " + cardFour);
