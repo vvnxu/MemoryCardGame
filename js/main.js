f99a7a98f1f9628ddaf1515b316645430b496021
vivien xu <lynchvivien@gmail.com>
2017-11-21 23:51:27 +1100

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
-console.log("Up and running!");
-var cardOne = "queen";
-console.log("User flipped " + cardOne);
-var cardTwo = "queen"
-console.log("User flipped " + cardTwo);
-var cardThree = "king";
-console.log("User flipped " + cardThree);
-var cardFour = "king";
-console.log("User flipped " + cardFour);
+var cards = ["queen","queen","king","king"];
+var cardInPlay = [];
+
+var checkForMatch = function(){
+	if (cardInPlay[0] === cardInPlay[1]){
+		alert("you found a match!");
+	} else {
+		alert("sorry,try again.");
+	}
+};
+
+var flipCard =function(cardId){
+	var flippedCard = cards[cardId];
+
+	cardInPlay.push(flippedCard);
+
+	console.log("User flipped " + flippedCard);
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
