3038e9cd019d99d89be7f8b6331865f80ea0c739
vivien xu <lynchvivien@gmail.com>
2017-11-23 00:28:24 +1100

    created more logic with Objects

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+	{name:"queen", rank:1,cardImage:"images/queen-of-diamonds.png",suit:"diamonds", },
+	{name:"queen",rank:2,cardImage:"images/queen-of-hearts.png",suit:"hearts",},
+	{name:"king",rank:3,cardImage:"images/king-of-diamonds.png",suit:"diamonds",},
+	{name:"king",rank:4,cardImage:"images/king-of-hearts.png",suit:"hearts",}
+];
 var cardInPlay = [];
 
 var checkForMatch = function(){
@@ -12,11 +17,12 @@ var checkForMatch = function(){
 var flipCard =function(cardId){
 	var flippedCard = cards[cardId];
 
-	cardInPlay.push(flippedCard);
+	cardInPlay.push(cards[cardId].name);
 
-	console.log("User flipped " + flippedCard);
+	console.log("User flipped " + cards[cardId].name);
+	console.log(cards[cardId].cardImage); console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+flipCard(1);
+flipCard(3);
